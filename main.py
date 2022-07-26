f33fd76626f79d7e2ea380595684685e33810ba9
whale <whalebox4.5@gmail.com>
2022-07-26 21:32:29 +0800

    clean up

--- main.py
+++ main.py
@@ -10,31 +10,8 @@ from discord.ext import commands
 keyword_file = open('keyword.json','r',encoding="utf-8")
 keyword_data = json.load(keyword_file)
 
-
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-
-
-
-'''
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if '你好機器人' in message.content:
-        await message.channel.send('嗶嗶，我是機器人，機油好難喝')
-'''
-
-
 
 bot = commands.Bot(command_prefix='/')
 
