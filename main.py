e2a46e8b42e4eccf5df57e1667daa2f28d2d4b33
whale <whalebox4.5@gmail.com>
2022-07-30 19:45:25 +0800

    file name change

--- main.py
+++ main.py
@@ -1,31 +1,79 @@
 import os
-
-import json
-
-import discord
 from dotenv import load_dotenv
 
-from discord.ext import commands
+import interactions
+import requests
+from bs4 import BeautifulSoup
 
-keyword_file = open('keyword.json','r',encoding="utf-8")
-keyword_data = json.load(keyword_file)
-
-
+import re
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='/')
+bot = interactions.Client(token=TOKEN, default_scope=False)
 
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
 
-@bot.command()
-async def pasta(ctx, arg):
-    for j in keyword_data['pasta']:
-        if arg in j['keywords']:
-            await ctx.send(j['value'])
+@bot.command(
+    name="win_deck",
+    description="顯示每周牌組餅圖與比例"
+)
+async def win_deck(ctx: interactions.CommandContext):
+    await ctx.send("請問你要選哪一個時段的上位餅圖？")
 
-bot.run(TOKEN)
+    webdata = requests.get(
+    "https://ntucgm.blogspot.com/search/label/OCG%E8%B3%BD%E4%BA%8B-Meta%E5%B1%95%E6%9C%9B/")
 
+    soup = BeautifulSoup(webdata.text, "html.parser")
+
+    sel = soup.findAll("h3", {'class': "entry-title"})
+
+    a = []
+    for s in sel:
+        a.append({
+            "link": s.findChild("a")["href"],
+            "title": str(s.find("div", {'class': 'r-snippetized'}).text).strip('\n')
+            })
+    option_str = ""
+    for x in a[:9]:
+        option_str+= str(a.index(x)+1) + ': ' + x['title'] + '\n'
+
+    week_menu = interactions.SelectMenu(
+        options=[
+            interactions.SelectOption(
+                label=str(a.index(x)+1) +': '+ x['title'],
+                value=x['link'],
+            )
+             for x in a[:9]    
+            ],
+        custom_id="week_select",
+    )    
+        
+    await ctx.send('```\n'+option_str+'\n```',components=week_menu)
+    
+
+@bot.component("week_select")
+async def week(ctx, value):
+
+    webdata = requests.get(value[0])
+
+    soup = BeautifulSoup(webdata.text, "html.parser")
+
+    sel = soup.find("div",{'class':'post'})
+
+    await ctx.send(sel.find_next('img')["src"])
+
+    sel = soup.find_all('b')
+
+    regex = re.compile("^.*[0-9](\s|)[0-9]+\.[0-9]\%.*")
+
+    b = ""
+
+    for s in sel:
+        rs = regex.search(s.text)
+        if rs is not None:
+            b+=str(rs.group(0)) + '\n'
+
+    await ctx.send('```\n'+str(b)+'\n```')
+
+
+bot.start()
